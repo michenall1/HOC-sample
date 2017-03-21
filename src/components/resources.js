d95855c0006923a855dca151e309a8f8fa016ac4
BowenLiu <bliu3535@gmail.com>
2017-03-21 12:13:00 -0700

    add resource

--- src/components/resources.js
+++ src/components/resources.js
@@ -1,3 +1,15 @@
 /**
  * Created by bowenliu on 2017/3/21.
- */
+ **/
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      Super Special Secret Recipe
+      <ul>
+        <li></li>
+      </ul>
+    </div>
+  )
+}
